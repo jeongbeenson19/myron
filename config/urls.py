ae4212c6cb9d15a387834ca7156cac461dcf0ca8
JeongbeenSon <jeongbeenson19@gmail.com>
2024-03-08 10:35:21 +0900

    로그인 기능 수정

--- config/urls.py
+++ config/urls.py
@@ -4,8 +4,7 @@ from django.urls import path, include
 from myron.views import myron_views
 
 urlpatterns = [
-    path('', auth_views.LoginView.as_view(template_name='auth.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='auth.html'), name='login'),
     path('admin/', admin.site.urls),
-    path('myron/', include('myron.urls')),
-    path('myron/', myron_views.index, name='index')
+    path('', myron_views.index, name='index')
 ]
--- myron/urls.py
+++ myron/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from .views import myron_views
 
+app_name = 'myron'
+
 urlpatterns = [
 ]
