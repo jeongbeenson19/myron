4a1cfa88898e120ffff286bc903333df94e67134
JeongbeenSon19 <jeongbeenson19@gmail.com>
2024-03-09 21:26:39 +0900

    캐시 관련 url 삭제

--- config/urls.py
+++ config/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='auth.html'), name='login'),
     path('admin/', admin.site.urls),
     path('', include('myron.urls')),
-    path('cache/', include('django.contrib.cache.urls'))
 ]
