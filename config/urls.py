13d1f372bd479c0c04745b78f62edc0877854fc7
JeongbeenSon19 <jeongbeenson19@gmail.com>
2024-03-09 22:31:55 +0900

    로그아웃 기능 구현

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='auth.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/login'), name='logout'),
     path('admin/', admin.site.urls),
     path('', include('myron.urls')),
 ]
